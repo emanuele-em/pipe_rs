451c6be31fb5b6d73461a45bf7484e8fc028746a
emanuele-em <micheletti.emanuele@hotmail.com>
2023-06-16 19:18:56 +0200

    add serialization and deserialization with protobuf

---
+++ build.rs
@@ -0,0 +1,6 @@
+extern crate prost_build;
+
+fn main() {
+    prost_build::compile_protos(&["src/raw_packet.proto"],
+                                &["src/"]).unwrap();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,170 +1,98 @@
-use std::iter;
-use std::os::unix::ffi::OsStrExt;
+use nix::NixPath;
+use tokio::net::unix::pipe::OpenOptions;
+
+use nix::sys::stat::Mode;
+use nix::unistd::mkfifo;
+use std::io;
+use std::path::Path;
 use std::process::Command;
+use tempfile::tempdir;
+use prost::Message;
+use std::io::Cursor;
 
-use anyhow::{anyhow, Context, Result};
-use async_trait::async_trait;
-use bincode::{Decode, Encode};
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio::net::unix::pipe;
-use tokio::sync::broadcast;
-use tokio::sync::mpsc::Sender;
-use tokio::sync::mpsc::{unbounded_channel, Receiver, UnboundedReceiver, UnboundedSender};
-use nix::{unistd::mkfifo, sys::stat::Mode};
-
-
-pub const CONF: bincode::config::Configuration = bincode::config::standard();
-pub const IPC_BUF_SIZE: usize = MAX_PACKET_SIZE + 4;
-
-#[derive(Decode, Encode, PartialEq, Eq, Debug)]
-pub enum MacosIpcRecv {
-    Packet {
-        data: Vec<u8>,
-        pid: u32,
-        process_name: Option<String>,
-    },
+pub mod raw_packet {
+    include!(concat!(env!("OUT_DIR"), "/pipe_rs.raw_packet.rs"));
 }
 
-#[derive(Decode, Encode, PartialEq, Eq, Debug)]
-pub enum MacosIpcSend {
-    Packet(Vec<u8>),
-    SetIntercept(InterceptConf),
+pub fn serialize_packet(raw_packet: &raw_packet::Packet) -> Vec<u8> {
+    let mut buf = Vec::new();
+    buf.reserve(raw_packet.encoded_len());
+    // Unwrap is safe, since we have reserved sufficient capacity in the vector.
+    raw_packet.encode(&mut buf).unwrap();
+    buf
 }
 
-pub struct MacosConf;
+pub fn deserialize_packet(buf: &[u8]) -> Result<raw_packet::Packet, prost::DecodeError> {
+    raw_packet::Packet::decode(&mut Cursor::new(buf))
+}
 
-#[async_trait]
-impl PacketSourceConf for MacosConf {
-    type Task = MacosTask;
-    type Data = UnboundedSender<MacosIpcSend>;
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let temp_dir = tempdir().unwrap();
+    let fifo_path = Path::new("/Users/emanuelemicheletti/Downloads/test.pipe");
+    //let fifo_path = temp_dir.path().join("test.pipe");
+    
 
-    fn name(&self) -> &'static str {
-        "Macos proxy"
+    // create new fifo and give read, write and execute rights to the owner
+    match mkfifo(fifo_path, Mode::S_IRWXU) {
+        Ok(_) => println!("created {:?}", fifo_path),
+        Err(err) => println!("Error creating fifo: {}", err),
     }
 
-    async fn build(
-        self,
-        net_tx: Sender<NetworkEvent>,
-        net_rx: Receiver<NetworkCommand>,
-        sd_watcher: broadcast::Receiver<()>,
-    ) -> Result<(MacosTask, Self::Data)> {
+    // now open the app and pass the fifo_path as arg
+    let executable_path = "/Applications/MitmproxyAppleTunnel.app/";
+    Command::new("open")
+        .arg("-a")
+        .arg(executable_path)
+        .arg("--args")
+        .arg(&fifo_path)
+        .spawn()
+        .expect("failed to execute process");
 
-        //create the pipe
-        let pipe_name = format!(
-            r"\\.\pipe\mitmproxy-transparent-proxy-{}",
-            std::process::id()
-        );
+    let rx = OpenOptions::new().open_receiver(&fifo_path).unwrap();
+    let tx = OpenOptions::new().open_sender(fifo_path).unwrap();
 
-        mkfifo(pipe_name.as_str(), Mode::S_IRWXU)?;
-
-        let ipc_server = PipeServer::new(&pipe_name)?;
-
-        let (conf_tx, conf_rx) = unbounded_channel();
-
-        Ok((
-            MacosTask {
-                ipc_server,
-                buf: [0u8; IPC_BUF_SIZE],
-                net_tx,
-                net_rx,
-                conf_rx,
-                sd_watcher,
-            },
-            conf_tx,
-        ))
-    }
-}
-
-pub struct PipeServer{
-    tx: pipe::Sender,
-    rx: pipe::Receiver,
-}
-impl PipeServer {
-    pub fn new(fifo_name: &str) -> Result<Self>{
-       Ok(PipeServer{
-            tx: pipe::OpenOptions::new().open_sender(fifo_name)?,
-            rx: pipe::OpenOptions::new().open_receiver(fifo_name)?,
-        })
-    }
-}
-
-pub struct MacosTask {
-    ipc_server: PipeServer,
-    buf: [u8; IPC_BUF_SIZE],
-
-    net_tx: Sender<NetworkEvent>,
-    net_rx: Receiver<NetworkCommand>,
-    conf_rx: UnboundedReceiver<MacosIpcSend>,
-    sd_watcher: broadcast::Receiver<()>,
-}
-
-#[async_trait]
-impl PacketSourceTask for MacosTask {
-    async fn run(mut self) -> Result<()> {
-        log::debug!("Waiting for IPC connection...");
-        // self.ipc_server.connect().await?;
-        log::debug!("IPC connected!");
-
+    // tokio::spawn(async move {
+        let mut msg = vec![0; 1024];
         loop {
-            tokio::select! {
-                // wait for graceful shutdown
-                _ = self.sd_watcher.recv() => break,
-                // pipe through changes to the intercept list
-                Some(cmd) = self.conf_rx.recv() => {
-                    assert!(matches!(cmd, MacosIpcSend::SetIntercept(_)));
-                    let len = bincode::encode_into_slice(&cmd, &mut self.buf, CONF)?;
-                    self.ipc_server.tx.try_write(&self.buf[..len])?;
+            println!("waiting for data"); 
+            // Wait for the pipe to be readable
+            rx.readable().await.unwrap();
+            println!("data is ready to be read"); 
+            // Try to read data, this may still fail with `WouldBlock`
+            // if the readiness event is a false positive.
+            match rx.try_read(&mut msg) {
+                Ok(n) => {
+                    println!("read: {:?}", msg);
+                    let (splitted_msg, _) = &msg.split_at(n);
+                    let raw_packet = deserialize_packet(splitted_msg).unwrap();
+                    println!("{:?}", raw_packet.title);
+                    msg.truncate(n);
+                    break;
                 },
-                // read packets from the IPC pipe into our network stack.
-                r = self.ipc_server.rx.read_exact(&mut self.buf) => {
-                    let len = r.context("IPC read error.")?;
-                    if len == 0 {
-                        // https://learn.microsoft.com/en-us/windows/win32/ipc/named-pipe-client
-                        // Because the client is reading from the pipe in message-read mode, it is
-                        // possible for the ReadFile operation to return zero after reading a partial
-                        // message. This happens when the message is larger than the read buffer.
-                        //
-                        // We don't support messages larger than the buffer, so this cannot happen.
-                        // Instead, empty reads indicate that the IPC client has disconnected.
-                        return Err(anyhow!("redirect daemon exited prematurely."));
-                    }
-                    let Ok((MacosIpcRecv::Packet { data, pid, process_name }, n)) = bincode::decode_from_slice(&self.buf[..len], CONF) else {
-                        return Err(anyhow!("Received invalid IPC message: {:?}", &self.buf[..len]));
-                    };
-                    assert_eq!(n, len);
-                    let Ok(mut packet) = IpPacket::try_from(data) else {
-                        log::error!("Skipping invalid packet: {:?}", &self.buf[..len]);
-                        continue;
-                    };
-                    // WinDivert packets do not have correct IP checksums yet, we need fix that here
-                    // otherwise smoltcp will be unhappy with us.
-                    packet.fill_ip_checksum();
-
-                    let event = NetworkEvent::ReceivePacket {
-                        packet,
-                        tunnel_info: TunnelInfo::Macos {
-                            pid,
-                            process_name,
-                        },
-                    };
-                    if self.net_tx.try_send(event).is_err() {
-                        log::warn!("Dropping incoming packet, TCP channel is full.")
-                    };
-                },
-                // write packets from the network stack to the IPC pipe to be reinjected.
-                Some(e) = self.net_rx.recv() => {
-                    match e {
-                        NetworkCommand::SendPacket(packet) => {
-                            let packet = MacosIpcSend::Packet(packet.into_inner());
-                            let len = bincode::encode_into_slice(&packet, &mut self.buf, CONF)?;
-                            self.ipc_server.tx.try_write(&self.buf[..len])?;
-                        }
-                    }
-                }
-            }
+                Err(e) if e.kind() == io::ErrorKind::WouldBlock => continue,
+                Err(e) => panic!("Error {}", e)
+            };
         }
+        // Ok(())
+    // });
 
-        log::info!("Macos OS proxy task shutting down.");
-        Ok(())
-    }
+    // tokio::spawn(async move {
+    //     tx.writable().await.unwrap();
+    //     loop {
+    //         match tx.try_write(b"hello world") {
+    //             Ok(n) => {
+    //                 println!("written: {:?}", n);
+    //                 break;
+    //             }
+    //             Err(e) if e.kind() == io::ErrorKind::WouldBlock => {
+    //                 continue;
+    //             }
+    //             Err(e) => {
+    //                 panic!("Error: {}", e)
+    //             }
+    //         }
+    //     }
+    // });
+    Ok(())
 }
